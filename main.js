ff7bb392278579de56c6a5464ad0dbb1c7e89ac5
Pratik-1997 <pratikkeluskar1997@gmail.com>
2021-09-08 17:31:58 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+// 7.11  7.12  7.13
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+for ( let i = 0 ; i < btnsOpenModal.length; i++)
+    console.log(btnsOpenModal[i].addEventListener
+        ('click',function(){
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');          
+        }))
+
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
+
+
+document.addEventListener('keydown',function(e){
+    
+    if(e.key === 'Escape'){  //Escape key
+        if(!modal.classList.contains('hidden')){
+            closeModal();
+        }
+    }
+})
